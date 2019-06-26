3d9b19b2e5273421243c7f8438d9e5ba73e18974
Charles Tapley Hoyt <cthoyt@gmail.com>
2019-06-26 22:57:32 +0200

    Move top level imports

--- src/embeddingdb/__init__.py
+++ src/embeddingdb/__init__.py
@@ -2,5 +2,4 @@
 
 """A package for storing and querying entity embeddings."""
 
-from .sql.io import upload_pykeen_from_directory, upload_word2vec, upload_word2vec_embedding_file
 from .version import get_version
--- src/embeddingdb/sql/__init__.py
+++ src/embeddingdb/sql/__init__.py
@@ -1,3 +1,5 @@
 # -*- coding: utf-8 -*-
 
 """A relational database structure for storing embeddings."""
+
+from .io import upload_pykeen_from_directory, upload_word2vec, upload_word2vec_embedding_file
